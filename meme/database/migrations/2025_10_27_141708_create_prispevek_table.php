5176eae37b7ec33f1171d8472f262e002353d32d
Luboš Dokulil <lubosd@office365.zcu.cz>
2025-10-27 16:14:19 +0100

    first commit

---
+++ meme/database/migrations/2025_10_27_141708_create_prispevek_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('prispevek', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_uzivatel');
+            $table->string('fotka');
+            $table->integer('lajky');
+            $table->integer('id_komentar');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('prispevek');
+    }
+};
